19bac505d74eb7b38ff92ba239a4792832df230d
Marcio <marcio.v.ordonez@gmail.com>
2023-10-18 21:36:04 -0300

    chore: move cpf to domain

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,9 @@
 use clap::Parser;
 use cli_clipboard::{ClipboardContext, ClipboardProvider};
-use rand::{thread_rng, Rng};
+
+use crate::domain::cpf::Cpf;
+
+pub mod domain;
 
 #[derive(Parser, Debug)]
 enum CliArgs {
@@ -17,7 +20,7 @@ fn main() {
 
     match args {
         CliArgs::Cpf(options) => {
-            let cpf: String = generate_cpf();
+            let cpf: String = Cpf::generate();
             match options.clip_board {
                 Some(true) => {
                     let mut clipboard =
@@ -33,23 +36,4 @@ fn main() {
             println!("{:?}", cpf);
         }
     }
-}
-
-fn generate_cpf() -> String {
-    let mut rng = thread_rng();
-    let n: Vec<u32> = (0..9).map(|_| rng.gen_range(0..9)).collect();
-    let mut k =
-        n[1] * 9 + n[2] * 8 + n[3] * 7 + n[4] * 6 + n[5] * 5 + n[6] * 4 + n[7] * 3 + n[8] * 2;
-    let mut j = 11 - ((n[0] * 10 + k) % 11);
-
-    j = if j >= 10 { 0 } else { j };
-
-    k = 11 - ((k + j) % 11);
-    k = if k >= 10 { 0 } else { k };
-
-    let cpf = format!(
-        "{:?}{:?}{:?}.{:?}{:?}{:?}.{:?}{:?}{:?}-{:?}{:?}",
-        n[0], n[1], n[2], n[3], n[4], n[5], n[6], n[7], n[8], j, k
-    );
-    cpf
 }
